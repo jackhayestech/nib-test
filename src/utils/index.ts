0904d4212f9fce0ac7147cc5f2e0cb50deafa0d8
Jack <contact@jackhayes.tech>
2022-11-29 14:48:04 +1100

    Order items function.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { loadData } from './loadData'
+export { orderProduct } from './orderProduct'
---
+++ src/utils/orderProduct.ts
@@ -0,0 +1,3 @@
+export const orderProduct = (productId: number, orderAmount: number) => {
+  console.log(`Product number: ${productId} has been ordered from our supplier for ${orderAmount} units.`)
+}
